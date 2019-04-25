6ca4578f9cc51cd11183440403b36a26e203b362
unknown <1469406888@mil>
2019-04-25 14:26:25 -0700

    added spotify ids

--- liri.js
+++ liri.js
@@ -4,13 +4,6 @@
 
 // ### Before You Begin
 // 1. LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
-// 2. Make a new GitHub repository called liri-node-app and clone it to your computer.
-// 3. To retrieve the data that will power this app, you'll need to send requests using the `axios` package to the Bands in Town, Spotify and OMDB APIs. You'll find these Node packages crucial for your assignment.
-//    * [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
-//    * [Axios](https://www.npmjs.com/package/axios)
-//      * You'll use Axios to grab data from the [OMDB API](http://www.omdbapi.com) and the [Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
-//    * [Moment](https://www.npmjs.com/package/moment)
-//    * [DotEnv](https://www.npmjs.com/package/dotenv)
 
 // ## Submission Guide
 // Create and use a standard GitHub repository. As this is a CLI App, it cannot be deployed to GitHub pages or Heroku. This time you'll need to include screenshots, a GIF, and/or a video showing us that you have the app working with no bugs. You can include these screenshots/GIFs or a link to a video in a `README.md` file.
@@ -28,10 +21,26 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+
+
+// 3. To retrieve the data that will power this app, you'll need to send requests using the `axios` package to the Bands in Town, Spotify and OMDB APIs. You'll find these Node packages crucial for your assignment.
+//    * [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+//    * [Axios](https://www.npmjs.com/package/axios)
+//      * You'll use Axios to grab data from the [OMDB API](http://www.omdbapi.com) and the [Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
+//    * [Moment](https://www.npmjs.com/package/moment)
+//    * [DotEnv](https://www.npmjs.com/package/dotenv)
+
+
+var axios = require("axios");
 
 var command = process.argv[2];
 var userInput = process.argv.slice(3).join(" ");
+
+//temp vars
+var movieName = "";
 
 switch (command) {
     case "concert-this": // 1. `node liri.js concert-this <artist/band name here>`
@@ -72,6 +81,9 @@ switch (command) {
         // * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
         // * It's on Netflix!
         // * You'll use the `axios` package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
+		movieName = userInput.replace(" ","+");
+		console.log(movieName);
+
         break;
 
     case "do-what-it-says": // 4. `node liri.js do-what-it-says`
