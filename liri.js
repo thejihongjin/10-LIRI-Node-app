0c447c1cd0d8bb51f6641296281a279ea5922713
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-05-01 19:54:35 -0700

    removed extra console logs

--- liri.js
+++ liri.js
@@ -1,14 +1,3 @@
-
-// ## Submission Guide
-// Create and use a standard GitHub repository. As this is a CLI App, it cannot be deployed to GitHub pages or Heroku. This time you'll need to include screenshots, a GIF, and/or a video showing us that you have the app working with no bugs. You can include these screenshots/GIFs or a link to a video in a `README.md` file.
-// * Include screenshots (or a GIF/Video) of the typical user flow of your application. Make sure to include the use of Spotify, Bands in Town, and OMDB.
-// * Include any other screenshots you deem necessary to help someone who has never been introduced to your application understand the purpose and function of it. This is how you will communicate to potential employers/other developers in the future what you built and why, and to show how it works.
-// * Because screenshots (and well-written READMEs) are extremely important in the context of GitHub, this will be part of the grading.
-// If you haven't written a markdown file yet, [click here for a rundown](https://guides.github.com/features/mastering-markdown/), or just take a look at the raw file of these instructions.
-
-
-
-
 require("dotenv").config();
 
 var keys = require("./keys.js");
@@ -27,7 +16,6 @@ var artist = "";
 var song = "";
 var movie = "";
 var queryUrl = "";
-var text = "";
 
 if (command === "do-what-it-says") { // node liri.js do-what-it-says
 	fs.readFile("random.txt", "utf8", function (err, data) {
@@ -39,12 +27,11 @@ if (command === "do-what-it-says") { // node liri.js do-what-it-says
 		command = dataArr[0];
 		userInput = dataArr[1];
 
-		doUserInput(command); // * It should run`spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+		doUserInput(command); // * it should run spotify-this-song for "I Want it That Way," as follows the text in random.txt file
 	});
 } else {
 	doUserInput(command);
 }
-
 
 function doUserInput(command) {
 	switch (command) {
@@ -61,22 +48,6 @@ function doUserInput(command) {
 			break;
 	}
 }
-
-
-// ### BONUS
-// * In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
-// * Make sure you append each command you run to the `log.txt` file. 
-// * Do not overwrite your file each time you run a command.
-function logData(text) {
-	fs.appendFile("log.txt", text, function (err) { // am I logging commands or results??
-		if (err) {
-			return console.log(err);
-		}
-	
-		console.log(text);
-	});
-}
-
 
 function concertThis() {
 	if (userInput !== "") {
@@ -106,7 +77,6 @@ function concertThis() {
 						"Location: " + location,
 						"Event date: " + date
 					].join("\n");
-					// console.log(concertData);
 					logData(concertData);
 				} else {
 					console.log("No events for this artist.")
@@ -126,14 +96,12 @@ function spotifyThisSong() {
 	}
 	spotify.search({ type: 'track', query: song }).then(
 		function (response) {
-			// console.log(response.tracks.items[0]);
 			var spotifyData = [
 				"Artist: " + response.tracks.items[0].artists[0].name,
 				"Song: " + response.tracks.items[0].name,
 				"Album: " + response.tracks.items[0].album.name,
 				"Preview: " + response.tracks.items[0].preview_url
 			].join("\n");
-			// console.log(spotifyData);
 			logData(spotifyData);
 		}
 	);
@@ -159,8 +127,16 @@ function movieThis() {
 				"Plot: " + response.data.Plot,
 				"Actors: " + response.data.Actors
 			].join("\n");
-			// console.log(movieData);
 			logData(movieData);
 		}
 	);
+}
+
+function logData(text) {
+	fs.appendFile("log.txt", text, function (err) {
+		if (err) {
+			return console.log(err);
+		}
+		console.log(text);
+	});
 }
